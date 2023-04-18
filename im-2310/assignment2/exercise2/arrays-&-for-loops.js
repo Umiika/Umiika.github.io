10552a86b355d0739826f7c85c1a7de2c9c76a1b
Umiika <127169522+Umiika@users.noreply.github.com>
2023-04-18 13:30:24 +1000

    Add files via upload

--- im-2310/assignment2/exercise2/arrays-&-for-loops.js
+++ im-2310/assignment2/exercise2/arrays-&-for-loops.js
@@ -8,5 +8,17 @@ var fruitArray = ["banana", "pineapple", "apple", "peach", "orange"];
 
 var mixedArray = [42, "pineapple", true, [3, 6, "nine"], "orange"];
 
-console.log("I'm going to help test the array");
+console.log(fruitArray[5]);
 
+fruitArray[1]="tomato"
+
+console.log(fruitArray,length);
+
+for(let fruit of fruitArray){
+    console.log(fruit);
+    if(fruit === "tomato"){
+        console.log("bur is it fruit?");
+    } else {
+        console.log(fruit);
+    }
+}

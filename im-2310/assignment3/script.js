38c35f501918ffac9532d230bf8ad148ff548e7a
Umiika <127169522+Umiika@users.noreply.github.com>
2023-06-02 16:38:24 +1000

    Add files via upload

---
+++ im-2310/assignment3/script.js
@@ -0,0 +1,35 @@
+document.getElementById("triangle").addEventListener("click", function(){
+    document.getElementById("triangleAudio").play();
+});
+
+document.getElementById("clave").addEventListener("click", function(){
+    document.getElementById("claveAudio").play();
+});
+
+document.getElementById("brush").addEventListener("click", function(){
+    document.getElementById("brushAudio").play();
+});
+
+document.getElementById("shaker").addEventListener("click", function(){
+    document.getElementById("shakerAudio").play();
+});
+
+document.getElementById("drum").addEventListener("click", function(){
+    document.getElementById("drumAudio").play();
+});
+
+document.getElementById("hihat").addEventListener("click", function(){
+    document.getElementById("hihatAudio").play();
+});
+
+document.getElementById("tambourine").addEventListener("click", function(){
+    document.getElementById("tambourineAudio").play();
+});
+
+document.getElementById("xylophone").addEventListener("click", function(){
+    document.getElementById("xylophoneAudio").play();
+});
+
+document.getElementById("gong").addEventListener("click", function(){
+    document.getElementById("gongAudio").play();
+});
